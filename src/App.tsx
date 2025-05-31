8a566dd8e51a80fa9d7e2e7fc91379e9789136a6
Abid Ibadurrahman <abid.i.ft20@mail.umy.ac.id>
2025-05-31 13:18:12 +0700

    refactor: remove React import as it's not needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Check, Heart, X } from "lucide-react";
 import Header from "./components/Header";
